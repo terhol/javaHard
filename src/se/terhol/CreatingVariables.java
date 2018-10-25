a9cc7f980446a0bfd3be8e5fd2d82bde8e87748b
Tereza Holm <holm.tereza@gmail.com>
2018-10-25 19:23:10 +0200

    Groovy replaced by JUnit 5

---
+++ src/se/terhol/CreatingVariables.java
@@ -0,0 +1,32 @@
+package se.terhol;
+
+public class CreatingVariables {
+    public static void main (String[] rgs){
+
+        int x, y, age;
+        double seconds, e, checking;
+        String firstName, lastName, title;
+
+        x = 10;
+        y = 400;
+        age = 39;
+
+        seconds = 4.71;
+        e = 2.71828182845904523536;
+        checking = 1.89;
+
+        firstName = "Graham";
+        lastName = "Mitchell";
+        title = "Mr.";
+
+        System.out.println("The variable x contains" + x);
+        System.out.println("The value" + y + " is stores in the variable y.");
+        System.out.println("The experiment took" + seconds + " seconds.");
+        System.out.println("A favourite irrational number is Euler's number:" + e);
+        System.out.println("Hopefully you have more than" + checking + "!");
+        System.out.println()
+
+
+
+    }
+}
