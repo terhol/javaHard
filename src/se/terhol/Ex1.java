6b5f71d2622cf95e66e02e8e2cab63528ac3e8f0
Tereza Holm <holm.tereza@gmail.com>
2018-10-25 19:23:51 +0200

    Groovy replaced by JUnit 5

---
+++ src/se/terhol/Ex1.java
@@ -0,0 +1,12 @@
+package se.terhol;
+
+public class Ex1 {
+
+    public static void main( String[] args ) {
+
+        System.out.println("I, Tereza, am determined to learn how to code!");
+        System.out.println("Today's date is October 25.");
+
+    }
+
+}
