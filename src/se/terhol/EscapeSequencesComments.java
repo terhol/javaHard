8eecd13001f797646927c65bd596029ac2bc943d
Tereza Holm <holm.tereza@gmail.com>
2018-10-25 19:23:23 +0200

    Groovy replaced by JUnit 5

---
+++ src/se/terhol/EscapeSequencesComments.java
@@ -0,0 +1,24 @@
+package se.terhol;
+
+public class EscapeSequencesComments {
+    public static void main (String[] args ) {
+
+        // This exercise demonstrates escape sequences and comments.
+        System.out.print("Learn\tJava\n\tthe\nhard\tway\n\n");
+        System.out.print("\tLearn Java the \"Hard\" Way!\n");
+        //System.out.frimp("Learn Java the hard way!");
+        System.out.print("Hello\n"); //This Line prints Hello
+        System.out.print("Jello\by\n"); // This Line prints Jelly
+        /* The quick brown fox jumped over a lazy dog.
+
+        Quick wafting zephyrs vex bold Jim. */
+        System.out /*  testing  */ .println ("Hard to believe, eh?");
+        System.out.println("Surprised? /* abcde */ Or what did you exepect?");
+        System.out.println("\\// -=- \\//");
+        System.out.println("\\\\ \\\\\\ \\\\\\\\"); //It takes 2 to make 1
+        System.out.print("I hope you understand \"escape sequences\" now.\n");
+        // and coments :)
+
+
+    }
+}
